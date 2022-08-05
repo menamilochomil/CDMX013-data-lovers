338015ba84e8324fec2e80ff9de8f0e765edd184
laladiaz <mariaabelardadiaz@gmail.com>
2022-08-05 17:52:42 -0500

    shows the spell data for harryPotter.js in the div id=spells using for DOM manipulation main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 //import { example } from './data.js';
 
-//import data from './data/harrypotter/harryPotter.js';
+import data from './data/harrypotter/harryPotter.js';
 
-//console.log(example, data);
+const dataSpells = data.spells;
+for (let i=0; i<dataSpells.length; i++) {
+   let spell = document.createElement("p");
+   spell.innerHTML = JSON.stringify(dataSpells[i]);
+   document.getElementById("spells").appendChild(spell);
+}
+
+
+//window.addEventListener("load", );
+/*function () {
+    //forEach para que itere sobre cada objeto de spells
+    //que cree un <p> por cada objeto de spells
+    //imprima en <div class="spells"> con innerHTML
+
+}*/
+
+
+    
+
+
