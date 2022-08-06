2a4eee69cf44ada2620ede059c9be9e1b0960cad
laladiaz <mariaabelardadiaz@gmail.com>
2022-08-05 19:22:58 -0500

    adds in main.js a for loop to iterate for each spell and print in DOM the name of spells

--- src/main.js
+++ src/main.js
@@ -3,22 +3,10 @@
 import data from './data/harrypotter/harryPotter.js';
 
 const dataSpells = data.spells;
+
 for (let i=0; i<dataSpells.length; i++) {
-   let spell = document.createElement("p");
-   spell.innerHTML = JSON.stringify(dataSpells[i]);
-   document.getElementById("spells").appendChild(spell);
+    let spell = document.createElement("p");
+    spell.innerHTML = JSON.stringify(dataSpells[i].name);
+    document.getElementById("spells").appendChild(spell);
 }
-
-
-//window.addEventListener("load", );
-/*function () {
-    //forEach para que itere sobre cada objeto de spells
-    //que cree un <p> por cada objeto de spells
-    //imprima en <div class="spells"> con innerHTML
-
-}*/
-
-
-    
-
 
