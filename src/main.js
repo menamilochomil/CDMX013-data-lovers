7aef8f8801ff06097427a1e0c75ca5ae9e24f126
laladiaz <mariaabelardadiaz@gmail.com>
2022-08-08 18:38:51 -0500

    adds the modal and its functions with click listener in main.js

--- src/main.js
+++ src/main.js
@@ -5,39 +5,47 @@ import data from './data/harrypotter/harryPotter.js';
 const dataSpells = data.spells;
 
 
-for (let i=0; i<dataSpells.length; i++) {
+for (let i = 0; i < dataSpells.length; i++) {
     //obtain the spell name from the database
-    let spellName = document.createElement("button");
-    spellName.setAttribute("class", "spell-name");
+    var spellName = document.createElement("button");
+    spellName.setAttribute("id", "spell-name");
     spellName.innerHTML = dataSpells[i].name;
     // prints the button in the grid
     document.getElementById("spells").append(spellName);
-    
+
     //create a div element for the modal
-    const spellModal = document.createElement("div");
-    spellModal.setAttribute("class", "spell-modal")
+    var spellModal = document.createElement("div");
+    spellModal.setAttribute("id", "spell-modal");
     //create a div element for the modal content
-    const spellContent = document.createElement("div");
-    spellContent.setAttribute("class", "spell-content");
+    var spellContent = document.createElement("div");
+    spellContent.setAttribute("id", "spell-content");
     //create a span element to close the modal
-    const closeSpell = document.createElement("span")
-    closeSpell.setAttribute("class", "close-spell")
-    closeSpell.textContent = "&times;"
+    var closeSpell = document.createElement("span");
+    closeSpell.setAttribute("id", "close-spell");
+    closeSpell.textContent = "&times;";
 
     //obtains the spell_type from the database
     let spellType = document.createElement("p");
-    spellType.setAttribute("class", "spell-type");
+    spellType.setAttribute("id", "spell-type");
     spellType.innerHTML = dataSpells[i].spell_type;
     //obtains the spell description from the database
     let spellDescription = document.createElement("p");
-    spellDescription.setAttribute("class", "spell-description")
+    spellDescription.setAttribute("id", "spell-description")
     spellDescription.innerHTML = dataSpells[i].description;
 
     spellContent.append(closeSpell, spellType, spellDescription);
-    spellModal.appendChild(spellContent);    
+    spellModal.append(spellContent);
+    document.getElementById("spells").appendChild(spellModal);
 }
 
-spellName.onclick = function () {
+spellName.addEventListener("click", function() {
     spellModal.style.display = "block"
-}
-
+})
+closeSpell.addEventListener("click", function () {
+    spellModal.style.display = "none"
+})
+window.addEventListener("click", function(event) {
+    if (event.target == spellModal) {
+        spellModal.style.display = "none"
+    }
+})
