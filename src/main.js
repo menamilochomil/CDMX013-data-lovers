c05286dfb4dcae26d13953726ba9a70c5e7b04a5
menamilochomil <jimenamilochomil@gmail.com>
2022-08-09 11:22:01 -0500

    Adds modal box with succes

--- src/main.js
+++ src/main.js
@@ -2,31 +2,49 @@
 
 import data from './data/harrypotter/harryPotter.js';
 
-data.potions.map(showPotions);
+
+
 function showPotions (item) {
-   return document.getElementById("potions").innerHTML += ` <div id="potions-container">
-            <span class= "potions-close">&times;</span>
-            <h4 class="potions-name">Name: ${item.name}</h4>
-            <p class="potions-content">Puntuación: ${item.description}</p><br>
-            </div>
-            <button id="btn-potions">${item.name}</button>
-            `;
+  const container = document.createElement('div')
+
+  let htmln = ` <div id="potions-container${item.id}" class="potions-container">
+  <div class="potions-container1">
+  <span class= "potions-close">&times;</span>
+  <h4 class="potions-name">Name: ${item.name}</h4>
+  <p class="potions-description">Puntuación: ${item.description}</p><br>
+  </div>
+  </div>
+  <button id="btn-potions${item.id}">${item.name}</button>
+  `;
+  container.innerHTML=htmln
+  container.querySelector(`#btn-potions${item.id}`).addEventListener('click', (e)=>{
+    e.preventDefault()
+    
+    let modal = document.getElementById(`potions-container${item.id}`);
+    modal.style.display = "block";
+    window.addEventListener("click", function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+  })
+   return container
     }
+data.potions.map((item)=>{
+  document.getElementById("potions").appendChild( showPotions(item))
+ });
 
 //I need to add a forEach to create a loop for passing the entire array in the functions below
 // Get the modal
-let modal = document.getElementById("potions-container");
 
-// Get the button that opens the modal
-let btn = document.getElementById("btn-potions");
+
+
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("potions-close")[0];
 
 // When the user clicks the button, open the modal 
-btn.addEventListener("click", function() {
-    modal.style.display = "block";
-});
+
 
 // When the user clicks on <span> (x), close the modal
 span.addEventListener("click", function() {
@@ -34,8 +52,3 @@ span.addEventListener("click", function() {
 });
 
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-});
