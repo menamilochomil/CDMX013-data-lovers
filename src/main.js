8ff4232a7d50ad9682ee36ac29581b875cf609fc
menamilochomil <jimenamilochomil@gmail.com>
2022-08-08 22:36:09 -0500

    Adds modal box in the format

--- src/main.js
+++ src/main.js
@@ -2,16 +2,40 @@
 
 import data from './data/harrypotter/harryPotter.js';
 
-
-const dataPotions = data.potions;
-
-dataPotions.map(showPotions);
-
-
+data.potions.map(showPotions);
 function showPotions (item) {
-   return document.getElementById("potions").innerHTML += (` <div class="potions-container">
-            <h4 class="potions-name">${item.name}</h4>
-            <p class="potions-description">Puntuación: ${item.description}</p><br>
+   return document.getElementById("potions").innerHTML += (` <div id="potions-container">
+            <span class= "potions-close">&times;</span>
+            <h4 class="potions-name">Name: ${item.name}</h4>
+            <p class="potions-content">Puntuación: ${item.description}</p><br>
             </div>
+            <button id="btn-potions">${item.name}</button>
             `);
     }
+
+
+// Get the modal
+var modal = document.getElementById("potions-container");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn-potions");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("potions-close")[0];
+
+// When the user clicks the button, open the modal 
+btn.addEventListener("click", function() {
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
