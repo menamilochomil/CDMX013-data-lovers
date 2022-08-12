c640bc3d1f5e588df929c01d054b14d50303e17f
laladiaz <mariaabelardadiaz@gmail.com>
2022-08-12 16:29:49 -0500

    cleans the code from comments

--- src/data.js
+++ src/data.js
@@ -1,32 +1,6 @@
-export const charactersFilterHouses = (arrData, house) => {
-
-  return arrData.filter(function (element) {
-
+export const charactersFilterHouses = function (arrData, house) {
+  let filterData = arrData.filter(function (element) {
     return element.house === house;
-    // if (selectedHouse.house === "Hufflepuff") {
-    //   return selectedHouse.house === "Hufflepuff";
-    // } else if (selectedHouse.house === "Ravenclaw") {
-    //   return selectedHouse.house === "Ravenclaw";
-    // } else if (selectedHouse.house === "Gryffindor") {
-    //   return selectedHouse.house === "Gryffindor";
-    // } else {
-    //   return selectedHouse.house === "Slytherin";
-    // }
   });
+  return filterData;
 }
-
-// export const charactersHufflepuff = dataCharacters.filter(function (selectedHouse) {
-//   return selectedHouse.house === "Hufflepuff";
-// });
-
-// export const charactersRavenclaw = dataCharacters.filter(function (selectedHouse) {
-//   return selectedHouse.house === "Ravenclaw";
-// });
-
-// export const charactersGryffindor = dataCharacters.filter(function (selectedHouse) {
-//   return selectedHouse.house === "Gryffindor";
-// });
-
-// export const charactersSlytherin = dataCharacters.filter(function (selectedHouse) {
-//   return selectedHouse.house === "Slytherin";
-// });
