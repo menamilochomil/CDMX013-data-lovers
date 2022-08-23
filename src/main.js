18d43a4b82c029b31ee02f38de1304c7fded7dc8
menamilochomil <jimenamilochomil@gmail.com>
2022-08-23 10:58:00 -0500

    Reorganizes the code to be easier to understand

--- src/main.js
+++ src/main.js
@@ -1,24 +1,31 @@
 import { charactersFilterHouses, filterHuman, filterNotHuman, sortZA, sortAZ, calculus, totalStudents, searchName } from './data.js';
 import data from './data/harrypotter/harryPotter.js';
 
-//Adding hide and appear function
-document.getElementById("characters-first-view").addEventListener("click", () =>{
+//User story 6: adding hide and appear function
+document.getElementById("characters-first-view").addEventListener("click", () => {
   document.getElementById("first-view").style.display = "none";
-  document.getElementById("characters-appear").style.display="block";
-    }
+  document.getElementById("characters-appear").style.display = "block";
+}
 )
-document.getElementById("spells-first-view").addEventListener("click", () =>{
+document.getElementById("spells-first-view").addEventListener("click", () => {
   document.getElementById("first-view").style.display = "none";
-  document.getElementById("spells-appear").style.display="block";
-    }
+  document.getElementById("spells-appear").style.display = "block";
+}
 )
-document.getElementById("potions-first-view").addEventListener("click", () =>{
+document.getElementById("potions-first-view").addEventListener("click", () => {
   document.getElementById("first-view").style.display = "none";
-  document.getElementById("potions-appear").style.display="block";
-    }
+  document.getElementById("potions-appear").style.display = "block";
+}
 )
 
-//Starting with other fucntions
+//random fun fact on load
+
+let funFacts = data.funFacts.splice(3, (data.funFacts).length);
+let randomFact = funFacts[Math.floor(Math.random() * funFacts.length)];
+document.getElementById("fun-facts").innerHTML = "Fun fact: " + randomFact.content
+
+//User story one
+//Display info in the web page: potions
 function showPotions(item) {
   const container = document.createElement('div')
 
@@ -39,14 +46,14 @@ function showPotions(item) {
     let modal = document.getElementById(`potions-container${item.id}`);
     modal.style.display = "block";
 
-    document.getElementById(`potions-close${item.id}`).addEventListener("click", function() {
+    document.getElementById(`potions-close${item.id}`).addEventListener("click", function () {
       modal.style.display = "none";
     });
 
     window.addEventListener("click", function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
-      } 
+      }
     });
   });
 
@@ -57,10 +64,10 @@ data.potions.map((item) => {
   document.getElementById("potions").appendChild(showPotions(item))
 });
 
-
-function showSpell (item) {
-    const container = document.createElement('div')
-    let htmlModal = ` 
+//Display info in the web page: spells
+function showSpell(item) {
+  const container = document.createElement('div')
+  let htmlModal = ` 
         <div id="spell-modal${item.id}" class="spell-modal">
             <div class="spell-content">
                 <span class= "spell-close" id="spell-close${item.id}">&times;</span>
@@ -95,6 +102,7 @@ data.spells.map((item) => {
   document.getElementById("spells").appendChild(showSpell(item))
 });
 
+//Display info in the web page: books
 function showBook(item) {
   const container = document.createElement('div')
   let htmlModal = ` 
@@ -126,17 +134,18 @@ function showBook(item) {
     });
 
   });
-    return container
+  return container
 }
 
 data.books.map((item) => {
   document.getElementById("books").appendChild(showBook(item))
 });
 
-   function showCharacters(item) {
-    const container = document.createElement('div');
-  
-    container.innerHTML = ` <div id="characters-container${item.id}" class="characters-container">
+//Display info in the web page: characters
+function showCharacters(item) {
+  const container = document.createElement('div');
+
+  container.innerHTML = ` <div id="characters-container${item.id}" class="characters-container">
     <div class="characters-content">
     <span class= "characters-close" id="characters-close${item.id}">&times;</span>
     <h4 class="characters-name">Name: ${item.name}</h4>
@@ -150,94 +159,96 @@ data.books.map((item) => {
     </div>
     <button id="btn-characters${item.id}" class="btn-characters"><strong>Name:</strong> ${item.name}</button>
     `;
-    
-    container.querySelector(`#btn-characters${item.id}`).addEventListener('click', (e) => {
-      e.preventDefault()
-  
-      let modal = document.getElementById(`characters-container${item.id}`);
-      modal.style.display = "block";
-  
-      document.getElementById(`characters-close${item.id}`).addEventListener("click", function() {
-        modal.style.display = "none";
-      });
-  
-      window.addEventListener("click", function (event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        } 
-      });
+
+  container.querySelector(`#btn-characters${item.id}`).addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let modal = document.getElementById(`characters-container${item.id}`);
+    modal.style.display = "block";
+
+    document.getElementById(`characters-close${item.id}`).addEventListener("click", function () {
+      modal.style.display = "none";
     });
-  
-    return container
-  }
-  
 
- function printCharacter(){ data.characters.map((item) => {
+    window.addEventListener("click", function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+
+  return container
+}
+
+
+function printCharacter() {
+  data.characters.map((item) => {
     document.getElementById("characters").appendChild(showCharacters(item))
-  })}
+  })
+}
 
-  printCharacter()
+printCharacter()
 
-// user story 2 houses
+// User story 2 houses
 
 function filterCharacters(e) {
-  // recognize the target
   document.getElementById("characters").innerHTML = "";
-  charactersFilterHouses(data.characters, e.target.id).forEach((item) =>{
+  charactersFilterHouses(data.characters, e.target.id).forEach((item) => {
     document.getElementById("characters").appendChild(showCharacters(item))
   });
-} 
+}
 
-document.querySelectorAll("li").forEach((li)=>{
-  li.addEventListener("click", (e) => { 
-    filterCharacters(e) 
-    document.getElementById("calculation").innerHTML ="Out of " + totalStudents(data.characters) +" students, " + calculus(data.characters, e.target.id) + "% are in " + e.target.id + " house.";
+document.querySelectorAll("li").forEach((li) => {
+  li.addEventListener("click", (e) => {
+    filterCharacters(e)
+    document.getElementById("calculation").innerHTML = "Out of " + totalStudents(data.characters) + " students, " + calculus(data.characters, e.target.id) + "% are in " + e.target.id + " house.";
     //user story 5: filter/sort
-    document.getElementById("reverse-button-characters").addEventListener("click", () =>{
+    document.getElementById("reverse-button-characters").addEventListener("click", () => {
       sortZA(filterCharacters(e))
-     }); 
-     document.getElementById("sort-button-characters").addEventListener("click", () =>{
+    });
+    document.getElementById("sort-button-characters").addEventListener("click", () => {
       sortAZ(filterCharacters(e))
     });
   });
 })
 
 
-//user 2 species
+//User story 2 species
 
- function showHuman(e){
+function showHuman(e) {
   //cleaning the grid of characters in DOM
-  document.getElementById("characters").innerHTML="";
-  //Recorre la función el array resultante de la función filterHuman e inserta cada character en el grid vaciado anteriormente.
-  filterHuman(data.characters, e.target.id).forEach ((item) => document.getElementById("characters").appendChild(showCharacters(item)))
+  document.getElementById("characters").innerHTML = "";
+  //For each element of the result in filter human function adds each character in the grid after cleaning
+  filterHuman(data.characters, e.target.id).forEach((item) => document.getElementById("characters").appendChild(showCharacters(item)))
 }
-//Ingresa el evento de escuchar el click y arrojar la función filter en showHuman
+//Adding event listener "click" and throwing the filter function in showHuman
 document.getElementById("Human").addEventListener("click", (e) => {
   showHuman(e)
   document.getElementById("calculation").innerHTML = "There are " + filterHuman(data.characters, e.target.id).length + " humans."
-   //user story 5: filter/sort
-   document.getElementById("reverse-button-characters").addEventListener("click", () =>{
+  //user story 5: filter/sort
+  document.getElementById("reverse-button-characters").addEventListener("click", () => {
     sortZA(showHuman(e))
-   }); 
-   document.getElementById("sort-button-characters").addEventListener("click", () =>{
+  });
+  document.getElementById("sort-button-characters").addEventListener("click", () => {
     sortAZ(showHuman(e))
   });
 })
 
-function showNotHuman(e){
-  //Vaciando el grid de characters en el DOM
-  document.getElementById("characters").innerHTML="";
-  //Recorre la función el array resultante de la función filterHuman e inserta cada character en el grid vaciado anteriormente.
-  filterNotHuman(data.characters, e.target.className).forEach ((item) => document.getElementById("characters").appendChild(showCharacters(item)))
+function showNotHuman(e) {
+  //cleaning DOM of the grid characters
+  document.getElementById("characters").innerHTML = "";
+  //For each element of the result in filter human function adds each character in the grid after cleaning
+  filterNotHuman(data.characters, e.target.className).forEach((item) => document.getElementById("characters").appendChild(showCharacters(item)))
 }
+
 document.querySelector(".Human").addEventListener("click", (e) => {
   showNotHuman(e)
   document.getElementById("calculation").innerHTML = "There are " + filterNotHuman(data.characters, e.target.className).length + " not humans."
-   //user story 5: filter/sort
-   document.getElementById("reverse-button-characters").addEventListener("click", () =>{
+  //user story 5: filter/sort
+  document.getElementById("reverse-button-characters").addEventListener("click", () => {
     sortZA(showNotHuman(e))
-   }); 
-   document.getElementById("sort-button-characters").addEventListener("click", () =>{
+  });
+  document.getElementById("sort-button-characters").addEventListener("click", () => {
     sortAZ(showNotHuman(e))
   });
 })
@@ -248,64 +259,68 @@ document.getElementById("clean-filter").addEventListener("click", () => {
   printCharacter()
 });
 
+//User story three
 //Sort from A to Z and Z to A for characters
 
-document.getElementById("sort-button-characters").addEventListener("click", () =>{
+document.getElementById("sort-button-characters").addEventListener("click", () => {
   document.getElementById("characters").innerHTML = ""
   document.getElementById("calculation").innerHTML = ""
-  sortAZ(data.characters).forEach((item) =>{
+  sortAZ(data.characters).forEach((item) => {
     document.getElementById("characters").appendChild(showCharacters(item))
-  })});
+  })
+});
 
-document.getElementById("reverse-button-characters").addEventListener("click", () =>{
+document.getElementById("reverse-button-characters").addEventListener("click", () => {
   document.getElementById("characters").innerHTML = ""
   document.getElementById("calculation").innerHTML = ""
-   sortZA(data.characters).forEach((item) =>{
-      document.getElementById("characters").appendChild(showCharacters(item))
-    })});
-  
+  sortZA(data.characters).forEach((item) => {
+    document.getElementById("characters").appendChild(showCharacters(item))
+  })
+});
+
 //Sort from A to Z and Z to A for spells
-document.getElementById("sort-button-spells").addEventListener("click", () =>{
+document.getElementById("sort-button-spells").addEventListener("click", () => {
   document.getElementById("spells").innerHTML = "";
-  sortAZ(data.spells).forEach((item) =>{
+  sortAZ(data.spells).forEach((item) => {
     document.getElementById("spells").appendChild(showSpell(item))
   });
 })
 
-  document.getElementById("reverse-button-spells").addEventListener("click", () => {
-    document.getElementById("spells").innerHTML = "";
-    sortZA(data.spells).forEach((item) =>{
+document.getElementById("reverse-button-spells").addEventListener("click", () => {
+  document.getElementById("spells").innerHTML = "";
+  sortZA(data.spells).forEach((item) => {
     document.getElementById("spells").appendChild(showSpell(item))
   });
 })
 
-
-document.getElementById("sort-button-potions").addEventListener("click", () =>{
+//Sort from A to Z and Z to A for potions
+document.getElementById("sort-button-potions").addEventListener("click", () => {
   document.getElementById("potions").innerHTML = "";
-  sortAZ(data.potions).forEach((item) =>{
+  sortAZ(data.potions).forEach((item) => {
     document.getElementById("potions").appendChild(showPotions(item))
   });
 })
 
-  document.getElementById("reverse-button-potions").addEventListener("click", () => {
-    document.getElementById("potions").innerHTML = "";
-    sortZA(data.potions).forEach((item) =>{
+document.getElementById("reverse-button-potions").addEventListener("click", () => {
+  document.getElementById("potions").innerHTML = "";
+  sortZA(data.potions).forEach((item) => {
     document.getElementById("potions").appendChild(showPotions(item))
   });
 })
 
-//search bar for  potions section
+//User story 4
+//search bar for potions section
 
 let searchPotions = document.getElementById("search-potions")
 let potionsForm = document.getElementById("search-potions-form")
 
 function searchPotionsResult() {
   document.getElementById("potions").innerHTML = "";
-  searchName(data.potions,searchPotions.value).forEach((item) => {
-  document.getElementById("potions").appendChild(showPotions(item))
+  searchName(data.potions, searchPotions.value).forEach((item) => {
+    document.getElementById("potions").appendChild(showPotions(item))
   });
 }
-potionsForm.addEventListener("submit", (e) =>{
+potionsForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchPotionsResult();
 })
@@ -315,39 +330,35 @@ potionsForm.addEventListener("keyup", (e) =>{
   e.preventDefault();
   searchPotionsResult();
 })*/
-// search bar of characters section
+
+// Search bar for characters
 
 let searchCharacters = document.getElementById("search-characters")
 let charactersForm = document.getElementById("search-characters-form")
 
 function searchCharactersResult() {
   document.getElementById("characters").innerHTML = "";
-  searchName(data.characters,searchCharacters.value).forEach((item) => {
-  document.getElementById("characters").appendChild(showCharacters(item))
+  searchName(data.characters, searchCharacters.value).forEach((item) => {
+    document.getElementById("characters").appendChild(showCharacters(item))
   });
 }
-charactersForm.addEventListener("submit", (e) =>{
+charactersForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchCharactersResult();
 })
-// search bar of spells section
+
+// Search bar for spells section
 let searchSpells = document.getElementById("search-spells")
 let spellsForm = document.getElementById("search-spells-form")
 
 function searchSpellsResult() {
   document.getElementById("spells").innerHTML = "";
-  searchName(data.spells,searchSpells.value).forEach((item) => {
-  document.getElementById("spells").appendChild(showSpell(item))
+  searchName(data.spells, searchSpells.value).forEach((item) => {
+    document.getElementById("spells").appendChild(showSpell(item))
   });
 }
-spellsForm.addEventListener("submit", (e) =>{
+spellsForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchSpellsResult();
 
 })
-
-//random fun fact on load
-
-let funFacts= data.funFacts.splice(3,(data.funFacts).length);
-let randomFact = funFacts[Math.floor(Math.random() * funFacts.length)];
-document.getElementById("fun-facts").innerHTML = "Fun fact: " + randomFact.content
